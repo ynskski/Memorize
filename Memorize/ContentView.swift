cf3000a1a415a02d3f7287457c1dd077b119e5f6
Yunosuke <qmkns246@gmail.com>
2020-09-17 21:55:26 +0900

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by 堺雄之介 on 2020/09/17.
+//  Copyright © 2020 堺雄之介. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
