8787a32b1e9d6a6dc048a4e2e227b261ce74eb40
Yunosuke Sakai <qmkns246@gmail.com>
2020-09-30 19:28:04 +0900

    Implement Cardify modifier and done Lecture 5.

---
+++ Memorize/Cardify.swift
@@ -0,0 +1,39 @@
+//
+//  Cardify.swift
+//  Memorize
+//
+//  Created by YunosukeSakai on 2020/09/30.
+//  Copyright © 2020 堺雄之介. All rights reserved.
+//
+
+import SwiftUI
+
+struct Cardify: ViewModifier {
+    var isFaceUp: Bool
+    
+    func body(content: Content) -> some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: cornerRadius)
+                    .fill(Color.white)
+                RoundedRectangle(cornerRadius: cornerRadius)
+                    .stroke(lineWidth: edgeLineWidth)
+                content
+            } else {
+                RoundedRectangle(cornerRadius: cornerRadius)
+                    .fill()
+            }
+        }
+    }
+    
+    // MARK: - Drawing Constants
+    
+    private let cornerRadius: CGFloat = 10.0
+    private let edgeLineWidth: CGFloat = 3
+}
+
+extension View {
+    func cardify(isFaceUp: Bool) -> some View {
+        self.modifier(Cardify(isFaceUp: isFaceUp))
+    }
+}
